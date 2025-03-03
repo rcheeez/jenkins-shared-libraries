cc86d8f950530eb9b7b6905a9b015f0ca4036558
Archies Gurav <93807923+rcheeez@users.noreply.github.com>
2025-03-03 17:33:47 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
 def call(String credentialId, String imageName, String tagName) {
   withCredentials([usernamePassword(credentialsId: "${credentialId}", usernameVariable: "dockerHubUser", passwordVariable: "dockerHubPass")]) {
-    sh "docker tag meetapp:latest ${env.dockerHubUser}/${imageName}:${tagName}"
+    sh "docker tag ${imageName}:${tagName} ${env.dockerHubUser}/${imageName}:${tagName}"
     sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
     sh "docker push ${env.dockerHubUser}/${imageName}:${tagName}"
   }
