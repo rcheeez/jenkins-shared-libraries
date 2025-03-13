fe82ca77ad2e1ad9201580778091f58d4585765a
Archies Gurav <93807923+rcheeez@users.noreply.github.com>
2025-03-13 13:39:46 +0530

    Update gitUpdateAndPush.groovy

--- vars/gitUpdateAndPush.groovy
+++ vars/gitUpdateAndPush.groovy
@@ -1,9 +1,11 @@
 def call(String url, String gitCredential) {
   withCredentials([gitUsernamePassword(credentialsId: "${gitCredential}", gitToolName: "Default")]){
     sh '''
+      git config --global user.name "rcheeez"
+      git config --global user.email "archiesgurav10@gmail.com"
       git add .
       git commit -m "Update: Updated Environment Variables"
-      git push ${url} main
+      git push ${url} dev
     '''
   }
 }
